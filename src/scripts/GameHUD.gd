d974e252c7afcc28df29bc008a14baf140e9a89d
Antonio Cambule <developer@cambule.info>
2023-06-04 16:53:00 +0200

    * Neue Assets für Traps * GameHud für Leben erweitert * Tilemap Collision Coins geprüft

--- src/scripts/GameHUD.gd
+++ src/scripts/GameHUD.gd
@@ -1,10 +1,15 @@
 extends CanvasLayer
 
 @onready var CoinsNode: Node = get_node("Control/CoinLabel")
+@onready var HeartNode: Node = get_node("Control/HeartLabel")
 
 func _ready() -> void:
 	CoinsNode.text = str(Globals.coins)
+	HeartNode.text = str(Globals.lives)
 
 
 func _on_coin_collected() ->void:
+	_ready()
+
+func _on_live_lost() ->void:
 	_ready()
